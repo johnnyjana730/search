55bc31f0645b8a321c682d0aabeef8f3ed47b0ae
Chang-Yu Tai <changyu.tai@eccalon.com>
2023-12-03 18:19:38 -0500

    check

--- botasaurus_v2/botasaurus/wait.py
+++ botasaurus_v2/botasaurus/wait.py
@@ -1,4 +1,10 @@
+# class Wait:
+#     SHORT = 4
+#     LONG = 8
+#     VERY_LONG = 16
+
+
 class Wait:
-    SHORT = 4
-    LONG = 8
-    VERY_LONG = 16
+    SHORT = 2
+    LONG = 4
+    VERY_LONG = 8
